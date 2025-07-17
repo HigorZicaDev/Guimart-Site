afd6a8bb9f84f2447e3b1bf1ef0236e5b1d20a4a
HigorZicaDev <hztech.dev.fr@gmail.com>
2025-07-17 12:45:08 +0200

    Fix: bugs and layout changing plus images carrossel

---
+++ assets/js/serviceImagens.js
@@ -0,0 +1,34 @@
+const serviceImages = {
+
+  banners: [
+    "https://placehold.co/800x400",
+    "https://placehold.co/800x400",
+    "https://placehold.co/800x400",
+  ],
+  plotagem: [
+    "../assets/images/services/plotagem/plotagem_1.jpg",
+    "../assets/images/services/plotagem/plotagem_2.jpg",
+    "../assets/images/services/plotagem/plotagem_3.jpg",
+  ],
+  adesivagem: [
+    "../assets/images/services/adesivagem/vitrini_1.jpg",
+    "../assets/images/services/adesivagem/vitrini_2.jpg",
+    "../assets/images/services/adesivagem/vitrini_3.jpg",
+  ],
+  placas: [
+    "https://placehold.co/800x400",
+    "https://placehold.co/800x400",
+    "https://placehold.co/800x400",
+  ],
+  design: [
+    "https://placehold.co/800x400",
+    "https://placehold.co/800x400",
+    "https://placehold.co/800x400",
+  ],
+  instalacao: [
+    "https://placehold.co/800x400",
+    "https://placehold.co/800x400",
+    "https://placehold.co/800x400",
+  ],
+
+};
